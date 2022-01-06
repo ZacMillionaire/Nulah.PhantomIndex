df96bed57f9d639f00aa47a2d2d3e052f74e3978
Scott <home@jotunga.com>
2022-01-07 09:54:24 +1000

    Added NulahInput control, and test data bindings

--- Nulah.PhantomIndex.WPF/Pages/Profiles/NewProfile.xaml.cs
+++ Nulah.PhantomIndex.WPF/Pages/Profiles/NewProfile.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Nulah.PhantomIndex.Lib.Images;
+using Nulah.PhantomIndex.WPF.ViewModels.Profiles;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -23,12 +24,13 @@ namespace Nulah.PhantomIndex.WPF.Pages.Profiles
     /// </summary>
     public partial class NewProfile : Page, INotifyPropertyChanged
     {
+        public NewProfileViewModel _viewModel = new();
+
         public NewProfile()
         {
             InitializeComponent();
+            DataContext = _viewModel;
         }
-
-
 
         public bool? FileDropValid
         {
@@ -100,6 +102,7 @@ namespace Nulah.PhantomIndex.WPF.Pages.Profiles
                 if (files.Length == 1)
                 {
                     ImageDropCanvas.Source = await ImageToBitmap(files[0]);
+                    _viewModel.FileName = files[0];
                 }
             }
 
@@ -131,6 +134,129 @@ namespace Nulah.PhantomIndex.WPF.Pages.Profiles
                 image.Freeze();
                 return image;
             });
+        }
+    }
+
+    public class NulahInput : UserControl
+    {
+        /// <summary>
+        /// Label
+        /// </summary>
+        public string Label
+        {
+            get { return (string)GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Label.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.Register(nameof(Label), typeof(string), typeof(NulahInput), new PropertyMetadata(null));
+
+        /// <summary>
+        /// Optional text to describe the input.
+        /// </summary>
+        public string Hint
+        {
+            get { return (string)GetValue(HintProperty); }
+            set { SetValue(HintProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for HintForeground.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HintProperty =
+            DependencyProperty.Register(nameof(Hint), typeof(string), typeof(NulahInput), new PropertyMetadata(null));
+
+        /// <summary>
+        /// Foreground colour for input hint
+        /// </summary>
+        public Brush HintForeground
+        {
+            get { return (Brush)GetValue(HintForegroundProperty); }
+            set { SetValue(HintForegroundProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for HintForeground.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HintForegroundProperty =
+            DependencyProperty.Register(nameof(HintForeground), typeof(Brush), typeof(NulahInput), new PropertyMetadata(null));
+
+
+        /// <summary>
+        /// Binding value for input value
+        /// </summary>
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(nameof(Text), typeof(string), typeof(NulahInput),
+                new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault)
+                {
+                    DefaultUpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
+                });
+
+        /// <summary>
+        /// If true, the binding source will be updated on any change to InputBox, otherwise change will occur on InputBox LostFocus
+        /// </summary>
+        public bool UpdateValueOnKeyUp
+        {
+            get { return (bool)GetValue(UpdateValueOnKeyUpProperty); }
+            set { SetValue(UpdateValueOnKeyUpProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for UpdateOnKeyUp.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty UpdateValueOnKeyUpProperty =
+            DependencyProperty.Register(nameof(UpdateValueOnKeyUp), typeof(bool), typeof(NulahInput), new PropertyMetadata(false, UpdateValueOnKeyUpPropertyChanged));
+
+        private static void UpdateValueOnKeyUpPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.NewValue != e.OldValue)
+            {
+                ((NulahInput)d).SetInputKeyUpHandler((bool)e.NewValue);
+            }
+        }
+
+        static NulahInput()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(NulahInput), new FrameworkPropertyMetadata(typeof(NulahInput)));
+        }
+
+        /// <summary>
+        /// Used for when <see cref="UpdateValueOnKeyUp"/> is true to raise binding updates on keydown
+        /// </summary>
+        private BindingExpression _textboxBindingExpression;
+
+        public override void OnApplyTemplate()
+        {
+            _textboxBindingExpression = BindingOperations.GetBindingExpression(GetTemplateChild("InputBox"), TextBox.TextProperty);
+
+            SetInputKeyUpHandler(UpdateValueOnKeyUp);
+            base.OnApplyTemplate();
+        }
+
+        /// <summary>
+        /// Adds/Removes <see cref="Input_KeyUp(object, KeyEventArgs)"/> from the InputBox's <see cref="UIElement.KeyUp"/> event
+        /// </summary>
+        /// <param name="addHandler"></param>
+        private void SetInputKeyUpHandler(bool addHandler)
+        {
+            if (GetTemplateChild("InputBox") is TextBox input && input != null)
+            {
+                if (addHandler == true)
+                {
+                    input.KeyUp += Input_KeyUp;
+                }
+                else
+                {
+                    input.KeyUp -= Input_KeyUp;
+                }
+            }
+        }
+
+        private void Input_KeyUp(object sender, KeyEventArgs e)
+        {
+            _textboxBindingExpression?.UpdateSource();
         }
     }
 }
---
+++ Nulah.PhantomIndex.WPF/ViewModels/Profiles/NewProfileViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Nulah.PhantomIndex.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nulah.PhantomIndex.WPF.ViewModels.Profiles
+{
+    public class NewProfileViewModel : ViewModelBase
+    {
+        private string _fileName;
+
+        public string FileName
+        {
+            get { return _fileName; }
+            set { NotifyAndSetPropertyIfChanged(ref _fileName, value); }
+        }
+    }
+}
